150d8a17ff9a02b60a801ed8eed8600a9fca9b3f
Moises Duarte <gladmd19@gmail.com>
2023-05-08 13:07:45 -0400

    updating routing

---
+++ src/app/pages/category/category.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+
+import { Product } from '../../models/product.model';
+import { ProductsService } from './../../services/products.service';
+
+@Component({
+  selector: 'app-category',
+  templateUrl: './category.component.html',
+  styleUrls: ['./category.component.scss']
+})
+export class CategoryComponent implements OnInit {
+
+  categoryId: string | null = null;
+  limit = 10;
+  offset = 0;
+  products: Product[] = [];
+
+  constructor(
+    private route: ActivatedRoute,
+    private productsService: ProductsService
+  ) { }
+
+  ngOnInit(): void {
+    this.route.paramMap
+    .pipe(
+      switchMap(params => {
+        this.categoryId = params.get('id');
+      if (this.categoryId) {
+        return this.productsService.getByCategory(this.categoryId, this.limit, this.offset)
+      }
+      return []
+      })
+    )
+    .subscribe((data) => {
+          this.products = data;
+        });
+      }
+      }
